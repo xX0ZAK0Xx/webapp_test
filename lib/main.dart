3a9c2f131435938679b9761d819d06c20f0c443a
zayed-m360 <zayedoyshik.m360@gmail.com>
2024-09-14 20:52:38 +0600

    url changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';  // ADD
+
+import 'src/navigation_controls.dart';                  // ADD
+import 'src/web_view_stack.dart';
 
 void main() {
   runApp(
@@ -18,6 +21,7 @@ class WebViewApp extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<WebViewApp> {
+  // Add from here...
   late final WebViewController controller;
 
   @override
@@ -28,16 +32,20 @@ class _WebViewAppState extends State<WebViewApp> {
         Uri.parse('https://abululayia-siddiqia.org'),
       );
   }
+  // ...to here.
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
+        // Add from here...
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+        // ...to here.
       ),
-      body: WebViewWidget(
-        controller: controller,
-      ),
+      body: WebViewStack(controller: controller),       // MODIFY
     );
   }
 }
