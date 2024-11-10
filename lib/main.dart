e49be0a7c05ad88a1e88f9f2c5a72dcfa0adf04c
zayed-m360 <zayedoyshik.m360@gmail.com>
2024-11-10 20:23:28 +0600

    Fixed Audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
-import 'package:webview_flutter/webview_flutter.dart';  // ADD
-
-import 'src/navigation_controls.dart';                  // ADD
-import 'src/web_view_stack.dart';
 import 'firebase_options.dart';
+import 'src/launch_webview.dart'; 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,78 +16,10 @@ Future<void> main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: const WebViewApp(),
+      home: const LaunchWebView(
+        title: "Tariqa Abululayia Siddiqia",
+        launchUrl: "https://abululayia-siddiqia.org",
+      ),
     ),
   );
-}
-
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
-
-  @override
-  State<WebViewApp> createState() => _WebViewAppState();
-}
-
-class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(
-        Uri.parse('https://abululayia-siddiqia.org'),
-      );
-  }
-
-  Future<bool> _onWillPop() async {
-    if (await controller.canGoBack()) {
-      await controller.goBack();
-      return false; // Prevent app from closing
-    } else {
-      return await _showExitConfirmationDialog();
-    }
-  }
-
-  Future<bool> _showExitConfirmationDialog() async {
-    return await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Exit App'),
-        content: const Text('Do you want to close the app?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(false), // Stay in the app
-            child: const Text('No'),
-          ),
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(true), // Close the app
-            child: const Text('Yes'),
-          ),
-        ],
-      ),
-    ) ?? false; // If the user dismisses the dialog, return false
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _onWillPop, // Handle back button press
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text('Tariqa Abululayia Siddiqia'),
-        ),
-        body: WebViewStack(controller: controller),  // Web view stack
-        bottomNavigationBar: BottomAppBar(
-          height: 60,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: NavigationControls(controller: controller),  // Navigation moved here
-          ),
-        ),
-      ),
-    );
-  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import flutter_inappwebview_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
