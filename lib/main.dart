0c3843bd52e72f6ae3805663ee9643549684767d
zayed-m360 <zayedoyshik.m360@gmail.com>
2024-11-13 22:53:12 +0600

    optimized

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'firebase_options.dart';
-import 'src/launch_webview.dart'; 
+import 'src/launch_webview.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/src/launch_webview.dart
+++ lib/src/launch_webview.dart
@@ -8,50 +8,44 @@ class LaunchWebView extends StatefulWidget {
   final String launchUrl;
   final String title;
 
-  const LaunchWebView(
-      {super.key, required this.launchUrl, required this.title});
+  const LaunchWebView({super.key, required this.launchUrl, required this.title});
 
   @override
   LaunchWebViewState createState() => LaunchWebViewState();
 }
 
 class LaunchWebViewState extends State<LaunchWebView> {
-  final GlobalKey webViewKey = GlobalKey();
+  late final InAppWebViewController? _webViewController;
+  late final PullToRefreshController _pullToRefreshController;
 
-  InAppWebViewController? webViewController;
-  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-        mediaPlaybackRequiresUserGesture: false,
-      ),
-      android: AndroidInAppWebViewOptions(
-        useHybridComposition: true,
-      ),
-      ios: IOSInAppWebViewOptions(
-        allowsInlineMediaPlayback: true,
-      ));
+  final ValueNotifier<String> _currentUrl = ValueNotifier<String>('');
+  final ValueNotifier<double> _progress = ValueNotifier<double>(0.0);
 
-  PullToRefreshController? pullToRefreshController;
-  String url = "";
-  double progress = 0;
-  final urlController = TextEditingController();
+  final InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      useShouldOverrideUrlLoading: true,
+      mediaPlaybackRequiresUserGesture: false,
+      clearCache: true,
+    ),
+    android: AndroidInAppWebViewOptions(useHybridComposition: true),
+    ios: IOSInAppWebViewOptions(allowsInlineMediaPlayback: true),
+  );
 
   @override
   void initState() {
     super.initState();
+    _currentUrl.value = widget.launchUrl;
 
-    url = widget.launchUrl;
-
-    pullToRefreshController = PullToRefreshController(
-      options: PullToRefreshOptions(
-        color: Colors.blue,
-      ),
+    _pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(color: Colors.blue),
       onRefresh: () async {
         if (Platform.isAndroid) {
-          webViewController?.reload();
+          await _webViewController?.reload();
         } else if (Platform.isIOS) {
-          webViewController?.loadUrl(
-              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+          final url = await _webViewController?.getUrl();
+          if (url != null) {
+            _webViewController?.loadUrl(urlRequest: URLRequest(url: url));
+          }
         }
       },
     );
@@ -59,13 +53,15 @@ class LaunchWebViewState extends State<LaunchWebView> {
 
   @override
   void dispose() {
-    webViewController?.stopLoading();
+    _webViewController?.stopLoading();
+    _currentUrl.dispose();
+    _progress.dispose();
     super.dispose();
   }
 
   Future<bool> _onWillPop() async {
-    if (await webViewController?.canGoBack() ?? false) {
-      webViewController?.goBack();
+    if (await _webViewController?.canGoBack() ?? false) {
+      _webViewController?.goBack();
       return false;
     } else {
       return await _showExitConfirmationDialog();
@@ -74,23 +70,23 @@ class LaunchWebViewState extends State<LaunchWebView> {
 
   Future<bool> _showExitConfirmationDialog() async {
     return await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Exit App'),
-        content: const Text('Do you want to close the app?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(false),
-            child: const Text('No'),
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Exit App'),
+            content: const Text('Do you want to close the app?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Yes'),
+              ),
+            ],
           ),
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(true),
-            child: const Text('Yes'),
-          ),
-        ],
-      ),
-    ) ??
-    false;
+        ) ??
+        false;
   }
 
   @override
@@ -103,73 +99,46 @@ class LaunchWebViewState extends State<LaunchWebView> {
           title: Text(widget.title),
         ),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Stack(
-                  children: [
-                    InAppWebView(
-                      key: webViewKey,
-                      initialUrlRequest: URLRequest(url: WebUri.uri(Uri.parse(url))),
-                      initialOptions: options,
-                      pullToRefreshController: pullToRefreshController,
-                      onWebViewCreated: (controller) {
-                        webViewController = controller;
-                      },
-                      onLoadStart: (controller, url) {
-                        setState(() {
-                          this.url = url.toString();
-                          urlController.text = this.url;
-                        });
-                      },
-                      androidOnPermissionRequest: (controller, origin, resources) async {
-                        return PermissionRequestResponse(
-                          resources: resources,
-                          action: PermissionRequestResponseAction.GRANT,
-                        );
-                      },
-                      shouldOverrideUrlLoading: (controller, navigationAction) async {
-                        var uri = navigationAction.request.url;
-
-                        if (!["http", "https", "file", "chrome", "data", "javascript", "about"]
-                            .contains(uri?.scheme)) {
-                          if (await canLaunchUrl(Uri.parse(url))) {
-                            await launchUrl(Uri.parse(url));
-                            return NavigationActionPolicy.CANCEL;
-                          }
-                        }
-
-                        return NavigationActionPolicy.ALLOW;
-                      },
-                      onLoadStop: (controller, url) async {
-                        pullToRefreshController?.endRefreshing();
-                        setState(() {
-                          this.url = url.toString();
-                          urlController.text = this.url;
-                        });
-                      },
-                      onLoadError: (controller, url, code, message) {
-                        pullToRefreshController?.endRefreshing();
-                      },
-                      onProgressChanged: (controller, progress) {
-                        if (progress == 100) {
-                          pullToRefreshController?.endRefreshing();
-                        }
-                        setState(() {
-                          this.progress = progress / 100;
-                        });
-                      },
-                      onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                        setState(() {
-                          this.url = url.toString();
-                          urlController.text = this.url;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          child: InAppWebView(
+            initialUrlRequest:
+                URLRequest(url: WebUri.uri(Uri.parse(_currentUrl.value))),
+            initialOptions: options,
+            pullToRefreshController: _pullToRefreshController,
+            onWebViewCreated: (controller) {
+              _webViewController = controller;
+            },
+            onLoadStart: (controller, url) {
+              if (url != null) _currentUrl.value = url.toString();
+            },
+            onLoadStop: (controller, url) async {
+              _pullToRefreshController.endRefreshing();
+              if (url != null) _currentUrl.value = url.toString();
+            },
+            onProgressChanged: (controller, progress) {
+              _progress.value = progress / 100;
+              if (progress == 100) {
+                _pullToRefreshController.endRefreshing();
+              }
+            },
+            shouldOverrideUrlLoading:
+                (controller, navigationAction) async {
+              final uri = navigationAction.request.url;
+              if (uri != null &&
+                  !["http", "https", "file", "chrome", "data", "javascript", "about"]
+                      .contains(uri.scheme)) {
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri);
+                  return NavigationActionPolicy.CANCEL;
+                }
+              }
+              return NavigationActionPolicy.ALLOW;
+            },
+            androidOnPermissionRequest: (controller, origin, resources) async {
+              return PermissionRequestResponse(
+                resources: resources,
+                action: PermissionRequestResponseAction.GRANT,
+              );
+            },
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -190,16 +159,16 @@ class LaunchWebViewState extends State<LaunchWebView> {
           onTap: (index) async {
             switch (index) {
               case 0:
-                if (await webViewController?.canGoBack() ?? false) {
-                  webViewController?.goBack();
+                if (await _webViewController?.canGoBack() ?? false) {
+                  _webViewController?.goBack();
                 }
                 break;
               case 1:
-                webViewController?.reload();
+                _webViewController?.reload();
                 break;
               case 2:
-                if (await webViewController?.canGoForward() ?? false) {
-                  webViewController?.goForward();
+                if (await _webViewController?.canGoForward() ?? false) {
+                  _webViewController?.goForward();
                 }
                 break;
             }
